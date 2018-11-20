6ff4a1dfc6fe8ae22684131293ac33c38c300b45
APCS2nd <APCS2nd@MHS-Dreyer-01>
2018-11-20 09:50:58 -0800

    Continued Work on Checkpoint 2

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -5,17 +5,13 @@ public class FracCalc {
     public static void main(String[] args) {
         // TODO: Read the input from the user and call produceAnswer with an equation
     	Scanner userInput = new Scanner(System.in);
-    	/*System.out.print("Would you like to initiate Frac Calc program? (Type \"yes\" or \"no\"):");
-    	String contProgram = userInput.next().toLowerCase();
-    	if (contProgram.contentEquals("yes") == true) System.out.println("Welcome to the Frac Calc");
-    	while (contProgram.contentEquals("quit") != true) {
-    		System.out.print("Please enter fractions: ");
-    		String fracString = userInput.next();
-    		produceAnswer(fracString);
-    		System.out.println("\nDo you want to continue Frac Calc? (Type \"quit\" to end)");
-    		contProgram = userInput.next();
-    	}*/
+    	System.out.print("Welcome to the Frac Calc\nPlease enter expression to evaluate or type \"quit\" to exit Frac Calc: ");
     	String fracString = userInput.next();
+    	while (fracString.contentEquals("quit") != true) {
+    		produceAnswer(fracString);
+    		System.out.println("Please enter expression to evaluate or type \"quit\" to exit Frac Calc: ");
+    		fracString = userInput.next();
+    	}
     	System.out.println(produceAnswer(fracString));
     }
     
@@ -36,4 +32,20 @@ public class FracCalc {
 
     // TODO: Fill in the space below with any helper methods that you think you will need
     
+    public static int[] convertToInt(String input) {
+    	int[] fraction = new int[3];
+    	int underscoreIndex = input.indexOf("_");
+    	if (underscoreIndex != -1) {
+    		fraction[0] = Integer.parseInt(input.substring(0, underscoreIndex));
+    		underscoreIndex++;
+    	}
+    	else {
+    		fraction[0] = 0;
+    	}
+    	int dashIndex = input.indexOf("/");
+    	if (dashIndex == -1) {
+    		
+    	}
+    	
+    }
 }
